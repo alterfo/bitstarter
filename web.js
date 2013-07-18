f77f6a859caa222bd4d4bbc211a40f37840b71e0
Oleg <kalinon7@gmail.com>
2013-07-18 11:23:19 +0400

    styled bitstarter page

--- web.js
+++ web.js
@@ -10,8 +10,11 @@ var string = buf.toString();
 app.get('/', function(request, response) {
   response.send(string);
 });
+app.use("/static", express.static(__dirname+"/static"));
+
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
